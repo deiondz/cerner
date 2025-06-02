4dc2cedad3ef2a03264a4ac304a5d77f8c1a9535
Deion Dsouza <deiondsouza12@gmail.com>
2025-06-02 20:21:40 +0530

    Hotfix: useSearchParam error

--- src/app/dashboard/wards/page.tsx
+++ src/app/dashboard/wards/page.tsx
@@ -4,6 +4,8 @@ import WardStats from "./components/ward-stats";
 import { getWardStats } from "~/server/db/queries/get-ward-stats";
 import { Suspense } from "react";
 import WardStatsSkeleton from "./components/ward-stats-skeleton";
+import { Loader2 } from "lucide-react";
+
 export default async function WardsPage() {
   const workers = await getAllWorkers();
   const wards = await getWardStats();
@@ -13,7 +15,11 @@ export default async function WardsPage() {
       <Suspense fallback={<WardStatsSkeleton />}>
         <WardStats wards={wards} />
       </Suspense>
-      <WardTable workers={workers} />
+      <Suspense
+        fallback={<Loader2 className="text-muted-foreground animate-spin" />}
+      >
+        <WardTable workers={workers} />
+      </Suspense>
     </div>
   );
 }
