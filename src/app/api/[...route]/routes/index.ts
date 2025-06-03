52b7deaec9452619de173ac851057a8e3da9a5f5
Deion Dsouza <deiondsouza12@gmail.com>
2025-06-04 03:50:16 +0530

    Add route for ward management API in index.ts by importing and registering ward_routes.

--- src/app/api/[...route]/routes/index.ts
+++ src/app/api/[...route]/routes/index.ts
@@ -3,7 +3,8 @@ import { Hono } from "hono";
 // ** import router
 
 import { worker_routes } from "./workers";
-
+import { ward_routes } from "./wards";
 export const router = new Hono();
 
 router.route("/workers", worker_routes);
+router.route("/wards", ward_routes);
